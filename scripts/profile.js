664b41d30cb504704cd62125a021d0fc80adb19e
gmagana <german@finaldevstudio.com>
2017-05-23 10:30:29 -0400

    More beautify

--- scripts/profile.js
+++ scripts/profile.js
@@ -4,26 +4,17 @@ require('../server/globals')(global);
 require('colors');
 
 const CONSTS = require('fi-consts');
-
 const Chance = require('chance');
-const chance = new Chance();
-
-const WORD = {
-  length: 8
-};
-
-const OPTIONS = {
-  new: true
-};
-
 const mongoose = require('mongoose');
 const path = require('path');
 
-const connectionScript = require(path.join(__serverdir, 'config', 'database'));
+const chance = new Chance();
 
 mongoose.Promise = Promise;
 
 CONSTS.load(config('consts'));
+
+const connectionScript = require(path.join(__serverdir, 'config', 'database'));
 
 const profileFilePath = path.join(__serverdir, 'schemas', 'profile');
 const profileSchema = require(profileFilePath)(mongoose.Schema);
@@ -34,9 +25,17 @@ const userSchema = require(userFilePath)(mongoose.Schema);
 mongoose.model('profile', profileSchema);
 mongoose.model('user', userSchema);
 
-const Profile = mongoose.model('profile');
-const User = mongoose.model('user');
+const WORD = {
+  length: 8
+};
 
+const OPTIONS = {
+  new: true
+};
+
+/**
+ * Me so smart.
+ */
 function smartLog() {
   for (let i = 0; i < arguments.length; i++) {
     let argument = arguments[i];
@@ -48,11 +47,20 @@ function smartLog() {
   }
 }
 
+/**
+ *
+ * @param {*} length
+ */
 function randomAlpha(length) {
   WORD.length = length || WORD.length;
   return chance.word(WORD);
 }
 
+/**
+ *
+ * @param {*} gender
+ * @param {*} middle
+ */
 function randomName(gender, middle) {
   return chance.name({
     gender: gender || 'male',
@@ -60,157 +68,162 @@ function randomName(gender, middle) {
   });
 }
 
-const rdmEmail = randomAlpha(10) + '@mailinator.com';
-const rdmName = randomName();
+connectionScript().then(() => {
+  console.log('\nDoing stuff...');
 
-connectionScript()
-  .then(() => {
-    console.log('\nDoing stuff...');
+  /* Load Mongoose models */
+  const Profile = mongoose.model('profile');
+  const User = mongoose.model('user');
 
-    var data = {
-      name: rdmName,
-      email: rdmEmail,
-      password: '12345678',
-      gender: 'GENDER.MALE'
-    };
+  /* Generate random data values */
+  const rdmEmail = randomAlpha(10) + '@mailinator.com';
+  const rdmName = randomName();
 
-    return User.create(data)
+  var data = {
+    name: rdmName,
+    email: rdmEmail,
+    password: '12345678',
+    gender: 'GENDER.MALE'
+  };
 
-      .then((user) => {
-        console.log(user.hasProfiles);
+  return User.create(data)
 
-        var data = {
-          user: user._id,
-          email: rdmEmail,
-          functions: [{
-            name: 'Cortar pasto',
-            description: 'Cortar el pasto con mucho cuidado'
-          }, {
-            name: 'Regar',
-            description: 'Distribuir agua sobre la tierra'
-          }]
-        };
+    .then((user) => {
+      console.log(user.hasProfiles);
 
-        return Profile.create(data);
-      })
+      var data = {
+        user: user._id,
+        email: rdmEmail,
+        functions: [{
+          name: 'Cortar pasto',
+          description: 'Cortar el pasto con mucho cuidado'
+        }, {
+          name: 'Regar',
+          description: 'Distribuir agua sobre la tierra'
+        }]
+      };
 
-      /**
-       * Uncomment the following block to create another profile associated to the same user.
-       */
-      // .then((profile) => {
-      //   return Profile.create({
-      //     user: profile.user,
-      //     email: 'ai' + rdmEmail
-      //   });
-      // })
+      return Profile.create(data);
+    })
 
-      .then((profile) => {
-        return Profile.findOne().where('user').equals(profile.user).populate('user'); //The populate() method assigns the query matching document to the 'user' path of the profile.
-      })
+    /**
+     * Uncomment the following block to create another profile associated to the same user.
+     */
+    // .then((profile) => {
+    //   return Profile.create({
+    //     user: profile.user,
+    //     email: 'ai' + rdmEmail
+    //   });
+    // })
 
-      .then((profile) => {
+    .then(profile => Profile.findOne() //Implicit return (when no block, it assumes it is a return)
+      .where('user').equals(profile.user)
 
-        var query = {
-          user: profile.user
-        };
+      //The populate() method assigns the query matching document to the 'user' path of the profile.
+      .populate('user'))
 
-        var update = {
-          $push: {
-            functions: {
-              name: 'Pushear función',
-              description: 'Agregar una función al array de funciones'
-            }
+    .then((profile) => {
+
+      var query = {
+        user: profile.user
+      };
+
+      var update = {
+        $push: {
+          functions: {
+            name: 'Pushear función',
+            description: 'Agregar una función al array de funciones'
           }
-        };
+        }
+      };
 
-        return Profile.findOneAndUpdate(query, update, OPTIONS);
-      })
+      return Profile.findOneAndUpdate(query, update, OPTIONS);
+    })
 
-      .then((profile) => {
-        smartLog('hash: ', profile.hash);
-        smartLog('user name: ', profile.user.name);
-        smartLog('user has profiles', profile.user.hasProfiles);
-        smartLog('Upon creation, is profile enabled??: ', profile.isEnabled);
-        console.log('Profile functions so far:', profile.functions);
-        return profile;
-      })
+    .then((profile) => {
+      smartLog('hash: ', profile.hash);
+      smartLog('user name: ', profile.user.name);
+      smartLog('user has profiles', profile.user.hasProfiles);
+      smartLog('Upon creation, is profile enabled??: ', profile.isEnabled);
+      console.log('Profile functions so far:', profile.functions);
+      return profile;
+    })
 
-      .then((profile) => {
+    .then((profile) => {
 
-        var query = {
-          user: profile.user
-        };
+      var query = {
+        user: profile.user
+      };
 
-        var update = {
-          $pull: {
-            functions: {
-              name: 'Regar'
-            }
+      var update = {
+        $pull: {
+          functions: {
+            name: 'Regar'
           }
-        };
+        }
+      };
 
-        return Profile.findOneAndUpdate(query, update, OPTIONS);
-      })
+      return Profile.findOneAndUpdate(query, update, OPTIONS);
+    })
 
-      .then((profile) => {
-        console.log('\nProfile functions after pull:', profile.functions);
+    .then((profile) => {
+      console.log('\nProfile functions after pull:', profile.functions);
 
-        var query = {
-          user: profile.user
-        };
+      var query = {
+        user: profile.user
+      };
 
-        var update = {
-          $set: {
-            disabledAt: new Date()
-          }
-        };
+      var update = {
+        $set: {
+          disabledAt: new Date()
+        }
+      };
 
-        console.log('Profile will be disabled...............');
+      console.log('Profile will be disabled...............');
 
-        return Profile.findOneAndUpdate(query, update, OPTIONS);
-      })
+      return Profile.findOneAndUpdate(query, update, OPTIONS);
+    })
 
-      .then((profile) => {
-        console.log('Profile Enabled:', profile.isEnabled);
+    .then((profile) => {
+      console.log('Profile Enabled:', profile.isEnabled);
 
-        var query = {
-          user: profile.user
-        };
+      var query = {
+        user: profile.user
+      };
 
-        var update = {
-          $unset: {
-            disabledAt: 1
-          }
-        };
+      var update = {
+        $unset: {
+          disabledAt: 1
+        }
+      };
 
-        console.log('Profile will be enabled again............');
+      console.log('Profile will be enabled again............');
 
-        return Profile.findOneAndUpdate(query, update, OPTIONS);
-      })
+      return Profile.findOneAndUpdate(query, update, OPTIONS);
+    })
 
-      .then((profile) => {
-        console.log('Profile Enabled:', profile.isEnabled);
+    .then((profile) => {
+      console.log('Profile Enabled:', profile.isEnabled);
 
-        var query = {
-          'functions._id': profile.functions[0]._id
-        };
+      var query = {
+        'functions._id': profile.functions[0]._id
+      };
 
-        var update = {
-          $set: {
-            'functions.$.name': 'Cut the grass'
-          }
-        };
+      var update = {
+        $set: {
+          'functions.$.name': 'Cut the grass'
+        }
+      };
 
-        return Profile.findOneAndUpdate(query, update, OPTIONS);
-      })
+      return Profile.findOneAndUpdate(query, update, OPTIONS);
+    })
 
-      .then((profile) => {
-        console.log('UPDATE RESULT:', profile);
+    .then((profile) => {
+      console.log('UPDATE RESULT:', profile);
 
-        process.exit(0);
-      });
-  })
+      process.exit(0);
+    });
 
-  .catch((err) => {
-    console.log(err);
-  });
+}).catch((err) => {
+  console.log(err);
+});
